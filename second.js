733b03d61fbb1d4aafc8c04b028d0921fac6a079
scorpka <ylltrabott@mail.ru>
2019-10-19 14:38:50 +0000

    version 1.00

---
+++ second.js
@@ -0,0 +1,22 @@
+"use strict"
+// let yourMoney; let kolDeneg = 0;
+// yourMoney = prompt('skolko deneg', kolDeneg);
+// if (yourMoney <100){
+// 		alert (`vi  bomg`);
+
+// }else alert('vi bogach!');
+// let iBoss = confirm("Ты здесь главный?");
+// alert (isBoss);
+// var nameUser;
+// nameUser = prompt('first and second name',"" );
+// alert (nameUser);
+var userLogin; let name ='admin'; let parol;
+userLogin = prompt ('vvedite imya', );
+if (userLogin == name || userLogin == "админ"){
+		parol = prompt('vvedite parol', );
+		if (parol == 0){
+			confirm("otmeneno");
+		} else if (parol=="123"){
+			alert("zdorova xozyain");
+		}else alert("ya vas ne znay");
+}
