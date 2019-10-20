5f8d23c9af6da473af1d39b596a50b022cb5279e
scorpka <ylltrabott@mail.ru>
2019-10-20 12:28:08 +0000

    v1.06 functionChisla

--- second.js
+++ second.js
@@ -1,19 +1,42 @@
-"use strict"
+"use strict" 
+/*let d;
+	
+function minChislo(a,b) {
+		if (a > b) { d = "a bolshe b";
+			 }else if (a < b){ d = "a menshe b";
+			 }else	 d ="a ravno b";
+	return (d);
+}
+let pervoeChislo; let vtoroeChislo;
+pervoeChislo=prompt("vvedite pervoe chislo",);
+vtoroeChislo=prompt("vvedite vtoroe chislo",);
+ let abc =minChislo(pervoeChislo,vtoroeChislo);
+alert (abc);*/
 
-function abc(a)
+/*function pow(a,b)
 {
+	let summaChisel=a**b;
+	return summaChisel;
+}
+let pervoeChislo= prompt("vvedite 1 chislo",);
+let vtoroeChislo= prompt("vvedite 2 chislo",);
+let stepenChisla= pow(pervoeChislo,vtoroeChislo);
+alert(stepenChisla);*/
 
- 		
-	if (a <= 18){
-	a = "доступ закрыт";
-	return (a);} else 
-			{ 	
-				a ="Доступ открыт"; 
-					return (a);
-			}
+function ask(question, yes, no) {
+  if (+alert("da?",1)){ yes();
+  }else no();
 }
 
-let dostup;
-dostup = abc(19);
-//dostup =12;
-alert(dostup);
+function showOk() {
+  alert( "Вы согласны." );
+}
+
+function showCancel() {
+  alert( "Вы отменили выполнение." );
+}
+
+// использование: функции showOk, showCancel передаются в качестве аргументов ask
+ask("Вы согласны?", showOk, showCancel);
+let d = +alert("privet",12);
+alert(d);
