8c70a0a909c8b19380089031b4cab7035f838a58
scorpka <ylltrabott@mail.ru>
2019-10-23 15:50:38 +0000

    object

--- second.js
+++ second.js
@@ -1,42 +1,25 @@
 "use strict" 
-/*let d;
-	
-function minChislo(a,b) {
-		if (a > b) { d = "a bolshe b";
-			 }else if (a < b){ d = "a menshe b";
-			 }else	 d ="a ravno b";
-	return (d);
-}
-let pervoeChislo; let vtoroeChislo;
-pervoeChislo=prompt("vvedite pervoe chislo",);
-vtoroeChislo=prompt("vvedite vtoroe chislo",);
- let abc =minChislo(pervoeChislo,vtoroeChislo);
-alert (abc);*/
 
-/*function pow(a,b)
+/*function abc()
 {
-	let summaChisel=a**b;
-	return summaChisel;
-}
-let pervoeChislo= prompt("vvedite 1 chislo",);
-let vtoroeChislo= prompt("vvedite 2 chislo",);
-let stepenChisla= pow(pervoeChislo,vtoroeChislo);
-alert(stepenChisla);*/
+	alert ("vasia lox");
 
-function ask(question, yes, no) {
-  if (+alert("da?",1)){ yes();
-  }else no();
 }
 
-function showOk() {
-  alert( "Вы согласны." );
+abc();*/
+
+let parol = 
+{
+	name: "vasya",
+	password: "1234",
+	age: "32",
 }
 
-function showCancel() {
-  alert( "Вы отменили выполнение." );
-}
+//alert( parol.password );
 
-// использование: функции showOk, showCancel передаются в качестве аргументов ask
-ask("Вы согласны?", showOk, showCancel);
-let d = +alert("privet",12);
-alert(d);
+for (let x in parol) 
+{
+  //alert( key ); 
+  //alert( parol[x] ); 
+}
+alert(  parol.name === undefined);
