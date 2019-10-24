8255ff7d478a9f54bb89f2c80d37815fb335b3e0
scorpka <ylltrabott@mail.ru>
2019-10-24 18:46:39 +0000

    this2.0

--- second.js
+++ second.js
@@ -1,5 +1,5 @@
 "use strict"
-let obj2 = {
+/*let obj2 = {
   id: 1,
    name: "vasya",
     
@@ -14,3 +14,25 @@ function kakZvat() {
 }
 obj2.kakZvat =  kakZvat;
 obj2.kakZvat();
+*/
+var calculator = {
+	chislo1: 0,
+	chislo2: 0,
+	read: function(){
+	 	this.chislo1 = prompt("vvedite 1 chislo",);
+		 this.chislo2 = prompt("vvedite 2 chislo",);
+
+	},
+	sum: function(){
+			alert (+this.chislo1 -this.chislo2 +2*this.chislo2);
+
+	},
+	mullan: function(){
+			alert (this.chislo1 *this.chislo2);
+
+	}
+	
+}
+calculator.read();
+calculator.mullan();
+calculator.sum();
