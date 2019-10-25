29c689adfa4e2be574d50bd3e1074e4eb3f0df23
scorpka <ylltrabott@mail.ru>
2019-10-25 14:58:56 +0000

    calculator Constructor

--- second.js
+++ second.js
@@ -1,38 +1,23 @@
 "use strict"
-/*let obj2 = {
-  id: 1,
-   name: "vasya",
-    
-   }
- 
-//obj2.price = [1,2,3];
 
-//alert (obj2.price[1]);
-//obj2.kakZvat()=kakZvat();
-function kakZvat() {
-	alert(this.name);
-}
-obj2.kakZvat =  kakZvat;
-obj2.kakZvat();
-*/
-var calculator = {
-	chislo1: 0,
-	chislo2: 0,
-	read: function(){
-	 	this.chislo1 = prompt("vvedite 1 chislo",);
-		 this.chislo2 = prompt("vvedite 2 chislo",);
-
-	},
-	sum: function(){
-			alert (+this.chislo1 -this.chislo2 +2*this.chislo2);
-
-	},
-	mullan: function(){
-			alert (this.chislo1 *this.chislo2);
-
-	}
+function Calculator() {
 	
-}
+	this.read =  function(){
+	 	this.chislo1 = prompt("vvedite 1 chislo",0);
+		 this.chislo2 = prompt("vvedite 2 chislo",0);
+
+	};
+	this.sum = function(){
+		return	+this.chislo1 -this.chislo2 +2*this.chislo2;
+				
+	};
+	this.mullan = function(){
+		return this.chislo1 * this.chislo2;
+
+	};
+	
+};
+let calculator = new Calculator();
 calculator.read();
-calculator.mullan();
-calculator.sum();
+alert(calculator.mullan());
+alert(calculator.sum());
