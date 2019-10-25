fbe5e173405b791b7beab478513146a18c9d30bd
scorpka <ylltrabott@mail.ru>
2019-10-25 15:16:36 +0000

    accumulator

--- second.js
+++ second.js
@@ -1,23 +1,15 @@
 "use strict"
 
-function Calculator() {
-	
-	this.read =  function(){
-	 	this.chislo1 = prompt("vvedite 1 chislo",0);
-		 this.chislo2 = prompt("vvedite 2 chislo",0);
+function Accumulator(startingValue){
+	this.value = startingValue ;
+	this.read = function(){
+		this.chislo1 = +prompt("vvedite 1 chislo",0);
+		this.chislo2 = +prompt("vvedite 2 chislo",0);
+		this.value =  this.value + this.chislo1  + this.chislo2;
 
-	};
-	this.sum = function(){
-		return	+this.chislo1 -this.chislo2 +2*this.chislo2;
-				
-	};
-	this.mullan = function(){
-		return this.chislo1 * this.chislo2;
+	}
 
-	};
-	
-};
-let calculator = new Calculator();
-calculator.read();
-alert(calculator.mullan());
-alert(calculator.sum());
+}
+let accumulator = new Accumulator(1);
+accumulator.read();
+alert(accumulator.value);
