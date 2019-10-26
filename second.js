067cdfed1cd57ce54d4e87967233f86eabc441d9
scorpka <ylltrabott@mail.ru>
2019-10-26 11:36:49 +0000

    string otrezanie stroki

--- second.js
+++ second.js
@@ -1,15 +1,20 @@
 "use strict"
+/*var str = "sanya pidor voobshe pizdoc";
+//alert (str.indexOf('pidor'));
+alert( str.slice(3, 11) );
+alert( str.substr(2, 4) );alert( 'Österreich'.localeCompare('Zealand') ); 
+alert( String.fromCodePoint(90) );  Z*/
 
-function Accumulator(startingValue){
-	this.value = startingValue ;
-	this.read = function(){
-		this.chislo1 = +prompt("vvedite 1 chislo",0);
-		this.chislo2 = +prompt("vvedite 2 chislo",0);
-		this.value =  this.value + this.chislo1  + this.chislo2;
-
-	}
+function ucFirst(str){
+	let str0 = str.slice(0,1);
+	 str0 = str0.toUpperCase();
+	let str1 = str.substr(1,);
+	str = str0 + str1;
+	return  str;
 
 }
-let accumulator = new Accumulator(1);
-accumulator.read();
-alert(accumulator.value);
+let abc = ucFirst("zdorova");
+alert (ucFirst("zdorova"));
+ abc = "zdorova";
+alert (abc[0].toUpperCase() + abc.slice(1,));
+
