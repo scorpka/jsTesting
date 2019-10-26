df613316d981b3fee4982b4734d53cc410ff3723
scorpka <ylltrabott@mail.ru>
2019-10-26 12:03:51 +0000

    checkSpam

--- second.js
+++ second.js
@@ -5,16 +5,11 @@ alert( str.slice(3, 11) );
 alert( str.substr(2, 4) );alert( 'Österreich'.localeCompare('Zealand') ); 
 alert( String.fromCodePoint(90) );  Z*/
 
-function ucFirst(str){
-	let str0 = str.slice(0,1);
-	 str0 = str0.toUpperCase();
-	let str1 = str.substr(1,);
-	str = str0 + str1;
-	return  str;
-
+function checkSpam(str){
+	if (~str.indexOf('xxx') || ~str.indexOf("viagra") ||  ~str.indexOf("lsd")){
+		return "eto spam";
+	}else return str;
 }
-let abc = ucFirst("zdorova");
-alert (ucFirst("zdorova"));
- abc = "zdorova";
-alert (abc[0].toUpperCase() + abc.slice(1,));
 
+let abc = "ya prishla domoy,viagra vce horosho";
+alert(checkSpam(abc));
