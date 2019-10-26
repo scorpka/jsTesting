d6103ecda8d08d6e598e9e7e71a38645076e000d
scorpka <ylltrabott@mail.ru>
2019-10-26 12:18:27 +0000

    Yrezaem stroky

--- second.js
+++ second.js
@@ -5,11 +5,25 @@ alert( str.slice(3, 11) );
 alert( str.substr(2, 4) );alert( 'Österreich'.localeCompare('Zealand') ); 
 alert( String.fromCodePoint(90) );  Z*/
 
-function checkSpam(str){
+/*function checkSpam(str){
 	if (~str.indexOf('xxx') || ~str.indexOf("viagra") ||  ~str.indexOf("lsd")){
 		return "eto spam";
 	}else return str;
 }
 
 let abc = "ya prishla domoy,viagra vce horosho";
-alert(checkSpam(abc));
+alert(checkSpam(abc));*/
+
+function truncate(str, maxlength){
+	let	strLength = str.length ;
+	
+	if (strLength < maxlength || strLength == maxlength ){
+			return str;
+	
+	} else {
+					str = str.substr(0,maxlength) +"...";
+					return str;
+
+}
+}
+alert(truncate("zdorova viblyadki",6));
