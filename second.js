4fa5a61451154b5395be4b28bb8bf11dbc39d198
scorpka <ylltrabott@mail.ru>
2019-10-27 13:10:45 +0000

    summa array

--- second.js
+++ second.js
@@ -1,21 +1,20 @@
 "use strict"
-let abc = [];
-let summaVsex = 0;
-function sumInput() {
-		let abcd = 0;
- for  (;abcd < 10;abcd++){ 
-	 	
-		let importZnacheni = prompt ("vvedite znachenie или пустое значение для выхода.",);
-			if (importZnacheni == 0 || importZnacheni== undefined) {
-				break;	
-			} else {abc.push(importZnacheni); summaVsex = summaVsex + (+importZnacheni);}
-	};
+let arr = [1, -2, 3, 4, -9, 6];
+let sumChisel = 0;
+function getMaxSubSum() {
+	let i = arr.length;
+	let s4etchik = 0;
+	
+	for (i= i-1;i != -1 ; i--){
+		sumChisel = sumChisel + arr[i];
+		if (arr[i] >= 0) {
+			s4etchik = s4etchik+1;
+		}
+	}
+	if (s4etchik == 0){return sumchisel = 0;
+	}else return sumChisel;
 }
-
-sumInput();
-alert(abc);
-alert(summaVsex);
-
-
+getMaxSubSum();
+alert(sumChisel);
 
 
