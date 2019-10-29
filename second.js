84400052a2b886718919fd7ef4388ed7c1f77e4b
scorpka <ylltrabott@mail.ru>
2019-10-29 15:24:42 +0500

    calculator with exceptions

--- second.js
+++ second.js
@@ -1,31 +1,38 @@
 "use strict"
 
-let string = "my-short-string"; 
-function camelize(str){
-	let abbc = "";
-	let	abc =  str.split("-");
-	abc.forEach(function dde(item){
+/*
+*create function Calculator
+*create advancing function
+*execute
+*/
 
-		let mmnp = item[0].toUpperCase();
-			item =mmnp + item.slice(1,);
-		abbc = abbc + item;
-	});
-	return abbc;
+//функции
+function calculate() {
+	let firstOfNumber, secondOfNumber;
+	for (let abc = 0; abc != 10; abc++){
+		
+		 firstOfNumber = prompt("введите 1 число",);
+
+		
+		if ((firstOfNumber - 1 + 1) ==  firstOfNumber) {
+				break;
+				
+		};
+			confirm("vvedite chislo!"); 
+	}	
+	for (let abc = 0; abc != 10; abc++){
+		
+			secondOfNumber = prompt("введите 2 число",);
+		
+		if ((secondOfNumber - 1 + 1) ==  secondOfNumber) {
+				break;
+				
+		};
+	
 	}
-	
-          
-alert(camelize(string));
+return  (+firstOfNumber + +secondOfNumber);
+}
 
-
-
-
-
-	
-	
-
-
-
-
-
-
-
+//вывод
+let SummCalculator = calculate();
+alert (SummCalculator);
