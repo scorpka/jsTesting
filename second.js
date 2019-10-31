1b45b2bfdae07dfa765ce9c4e60d436760343b43
scorpka <ylltrabott@mail.ru>
2019-10-31 19:23:54 +0500

    timer.delayV1.01

--- second.js
+++ second.js
@@ -3,12 +3,16 @@
 
 function printNumbers(from, to) {
 	let timeId = setInterval(function abc() {
-			for (from = from; from <to; from++){
 			alert(from);
-						}},
+			from++;
+			if (from >= to) clearInterval(timeId);
+				},
 			1000)
 
 timeId;
+
 }
+ setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
 
 printNumbers(0, 10);
+setTimeout();
