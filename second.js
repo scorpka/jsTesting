2ec56c1d7c9414f4a682c00cffd2652eb19471c8
scorpka <ylltrabott@mail.ru>
2019-11-06 15:58:22 +0500

    palidrom

--- second.js
+++ second.js
@@ -1,42 +1,16 @@
 "use strict"
 
- function tickets(massiveNew) {
-	let sdacha25 = 0;
- 	let sdacha50 = 0;
- 	let polychka25 = 0;
- 	let polychka50 = 0;
- 	
- 	for (let i = 0; i < massiveNew.length; i++) {
- 			if (massiveNew[i] == 100) {
- 				sdacha50 = sdacha50+1;
- 				sdacha25 = sdacha25+1;
- 			}
- 			
- 			if (massiveNew[i] == 50) {
- 				sdacha25 = sdacha25+1;
- 				polychka50 = polychka50 + 1;
- 			}
- 			
- 			if (massiveNew[i] == 25) {
- 				polychka25 = polychka25+1;
- 			}
- 	}
- 	
-	if (sdacha25 == polychka25 || sdacha25 < polychka25) {
-		if (sdacha50 = polychka50 || sdacha50 < polychka50) {
-			alert("yes");
-		}else alert ("NO. Vasya will not have enough money to give change to 100 dollars");
+let stringUser = "test"; 
 
+function testPalindrom (stringUser) {
+		 let j = 0; 
 
-	}else if (polychka50 != 0 && sdacha50 != 0) { 
-		alert ("NO. Vasya will not have enough money to give change to your dollars");
- 	 }else if (sdacha50 == 0){
- 	 		alert ("NO. Vasya will not have enough money to give change to 50 dollars");
- 	 }else alert ("NO. Vasya will not have enough money to give change to 100 dollars");
- 	
+		for (let i = 0; i < stringUser.length; i++){
+				  let k = stringUser.length - i-1;
+		  if (stringUser[i] == (stringUser[k])){
+				j++;
+		  }else { alert(stringUser[i]); alert(k);
+		}
+} if(j == stringUser.length) alert(`your text - ${stringUser} is palindrom`)}
+testPalindrom("anna");
 
- 	
-}
-tickets([25,25,50]);
-tickets([25, 100]);
-tickets([25, 25, 50, 50, 100]);
