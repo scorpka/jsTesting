4a7306ba0afd9b2f30768b1c9f51177526ecb307
scorpka <ylltrabott@mail.ru>
2019-11-28 16:09:57 +0500

    unicalwords

--- second.js
+++ second.js
@@ -1,48 +1,16 @@
 "use strict" 
-//function deleting anagrams
-function anagramFilter(arr) {
-                
-        let whichDelete = "";
-        let cloneMass = arr; 
-        for (let i=0; i < arr.length; i++) {
-                let checker = arr[i]
-                for (let n=i+1; n < arr.length; n++){
-                        let checker2 = arr[n];
-                        
-                  let newMake =  wordFilter(checker,checker2)
-                if ( newMake == "true") {
-                       
-                      cloneMass.splice(n,1);
-                      cloneMass.splice(i,1);
-                }
-                };
-                 
+//massive
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+//function unical keys from massive
+function unique(arr){
+   let storageUni = new Set([]);
+        for (let j = 0; j < arr.length; j++) {
+                let wword = arr[j];
+                storageUni.add(wword); 
         }
-      return cloneMass;
+        return storageUni; 
 }
-//function finding anagrams
-function wordFilter (n,d) {
-
-    let postChecker = 0;
-    if ( n.length == d.length){
-
-      for (let j = 0; j < n.length; j++){
-       for (let e = 0; e < d.length; e++){  
-                                      
-         if (n[j] ===  d[e]){
-             postChecker = postChecker+1; 
-            if (postChecker ==(n.length-1)){
-                postChecker = 0;
-                    return  "true"; 
-             }
-         }
-                                
-       } 
-      };  
-      postChecker = 0; 
-    } 
-        
-}
-//execution
-let abc = ["vasya","a","bc","cde","ifjhi","fijhi","vasya"];
-alert (anagramFilter(abc));
+//execute
+console.log(unique(values));
