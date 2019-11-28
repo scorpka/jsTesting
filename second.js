47a5026710c9c419a9f72f1d0b7fa00d223c1904
scorpka <ylltrabott@mail.ru>
2019-11-28 21:05:47 +0500

    first recursion

--- second.js
+++ second.js
@@ -1,16 +1,10 @@
 "use strict" 
-//massive
-let values = ["Hare", "Krishna", "Hare", "Krishna",
-  "Krishna", "Krishna", "Hare", "Hare", ":-O"
-];
-//function unical keys from massive
-function unique(arr){
-   let storageUni = new Set([]);
-        for (let j = 0; j < arr.length; j++) {
-                let wword = arr[j];
-                storageUni.add(wword); 
-        }
-        return storageUni; 
+function factorial(n) {
+        if (n > 1) {
+                return n*factorial(n-1);
+        }else return n;
+        
 }
-//execute
-console.log(unique(values));
+alert(factorial(10));
+
+
